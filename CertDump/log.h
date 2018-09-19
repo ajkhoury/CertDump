3bd0e9d7f1583475404dfb6638a46cd2575ce728
dude719 <aidankhoury@gmail.com>
2018-09-19 09:16:56 -0300

    Add licensing.

--- CertDump/log.h
+++ CertDump/log.h
@@ -1,3 +1,22 @@
+/**
+ * CertDump
+ * Copyright (c) 2018 Aidan Khoury (dude719)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @file log.h
+ * @author Aidan Khoury (dude719)
+ * @date 9/17/2018
+ */
+
 #ifndef _LOG_H_
 #define _LOG_H_
 
